7bfb52b7d455d0418625f84b8c994cb7537b240d
Utiwari12 <motioniitjeeinstitute@gmail.com>
2024-08-26 12:54:55 +0530

    Datatypes

---
+++ 01-basic/02-dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // for ES6 treat all code as newer version
+
+//alert(3+5); we are using nodejs, not browser
+
+//console.log(3 + 5); // 8
+
+//console.log("Upendra Tiwari"); // Upendra Tiwari
+
+//console.log(3 + 5 + " Upendra Tiwari"); // 8Upendra Tiwari
+
+let name = "Upendra Tiwari";
+
+let age = 25;
+
+let isLoggedIn = true;
+let state = null
+
+//types of data
+
+//number => integer, float, bigint
+//bigint => 10n
+//boolean => true, false
+//string => "", '', ``
+//undefined => variable is not defined
+//null => null, object, standalone value
+//symbol => symbol, unique value
+//object => array, object, function
+
+console.log(typeof name, typeof age, typeof isLoggedIn, typeof state, typeof null, typeof undefined);
+console.log(typeof null);
+console.log(typeof undefined);
