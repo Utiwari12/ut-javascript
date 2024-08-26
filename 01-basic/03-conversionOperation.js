ad72822dacc63b66318ce69e426998c6a29c50a3
Utiwari12 <motioniitjeeinstitute@gmail.com>
2024-08-26 13:23:31 +0530

    Conversion of Data types

---
+++ 01-basic/03-conversionOperation.js
@@ -0,0 +1,57 @@
+//let score = '10ad';
+
+//let score = '10';
+
+//let score = null;
+
+//let score = undefined;
+
+//let score = true;
+
+let score = false;
+
+//Note for remember
+
+//"10" => 10
+//"10ad" => NaN
+// true => 1
+// false => 0
+
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+
+//let valueInString = String(valueInNumber);
+//console.log(typeof valueInString)
+//console.log(valueInString) // o/p NaN
+//console.log(valueInNumber) // o/p NaN
+
+//let isLoggedIn = 1
+//let isLoggedIn = 0
+
+//let isLoggedIn = ""
+
+//let isLoggedIn = "Upendra Tiwari";
+
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+//notes
+
+// 0 => false
+// 1 => true
+// "" => false
+// "Upendra Tiwari" => true
+
+let x = 20;
+
+let stringNumber = String(x);
+
+console.log(stringNumber , typeof stringNumber)
