e81c297e4eb85beca4f3823c1681d2820fc64f6f
Utiwari12 <motioniitjeeinstitute@gmail.com>
2024-08-26 16:37:59 +0530

    comparision

--- 01-basic/03-conversionOperation.js
+++ 01-basic/03-conversionOperation.js
@@ -50,8 +50,50 @@ let score = false;
 // "" => false
 // "Upendra Tiwari" => true
 
-let x = 20;
+//let x = 20;
 
-let stringNumber = String(x);
+//let stringNumber = String(x);
 
-console.log(stringNumber , typeof stringNumber)
+//console.log(stringNumber , typeof stringNumber)
+
+// *****************Operations on number ******************
+
+//let value = 10;
+//let newValue= -value;
+//console.log(newValue)
+
+// console.log(0 / 0)
+// console.log(2/3);
+// console.log(4/3);
+// console.log(4*3);
+// console.log(4+3);
+// console.log(4-3);
+// console.log(4%3);
+// console.log(4**3);
+// console.log(7%3);
+
+// let str1 = "Hello";
+
+// let str2 = "World";
+
+// let str3 = str1 + " " + str2;
+
+// console.log(str3)
+
+// console.log("10" + 10);
+// console.log("10" + 10 + 20);
+// console.log(10 + 10 + "10");
+
+// console.log(+true);
+// console.log(+false);
+// console.log(-true);
+// console.log(-false)
+//console.log(true+); //wrong code
+
+let gameCounter = 50
+gameCounter++
+++gameCounter
+console.log(gameCounter)
+
+// link to study material
+//https://tc39.es/ecma262/multipage/abstractions.html#sec-type-conversion
---
+++ 01-basic/04-comparision.js
@@ -0,0 +1,30 @@
+// console.log(10 == 10); //true
+// console.log(10 == "10"); //true
+
+// console.log(10 != 10); //false
+// console.log(10 != "10"); //false
+console.log(("2" > 1));
+console.log(("2" < 1)); //false
+
+console.log(null > 1); //false
+console.log(null < 1); //false
+console.log(null >0); //false
+console.log(null < 0); //false
+
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
+console.log(undefined == 0); //false
+
+console.log("10" === 10); //false
+console.log("10" !== 10); //true
+// Note to remember
+// ===  strictly equal
+// !==  strictly not equal
+// ==  loose equal
+// !=  loose not equal
+// >  greater than
+// <  less than
+// >=  greater than or equal
+// <=  less than or equal
+// &&  and gate
+// ||  or gate
