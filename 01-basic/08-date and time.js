4ce85bc2734ed27314b0f5fde000c650aad8b679
Utiwari12 <motioniitjeeinstitute@gmail.com>
2024-08-26 23:11:33 +0530

    Date and Time

---
+++ 01-basic/08-date and time.js
@@ -0,0 +1,40 @@
+// Dates
+
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let mycustomDate = new Date("2020-01-01");
+// console.log(mycustomDate);
+// console.log(mycustomDate.toDateString());
+// console.log(mycustomDate.toTimeString());
+// console.log(mycustomDate.toLocaleDateString());
+// console.log(typeof mycustomDate);
+// console.log(mycustomDate.toLocaleString());
+
+// Timestamps
+
+let myDate = new Date();
+
+let myTimestamp = Date.now();
+// console.log(myTimestamp);
+// console.log(typeof myTimestamp)
+// console.log(myDate.getTime());
+// console.log(Date.now());
+// console.log(Math.floor(Date.now() / 1000));
+
+// let newDate = new Date(myTimestamp);
+//  console.log(newDate);
+
+let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getTime());
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
+// console.log(newDate.getMonth()+1);
+
+
+
