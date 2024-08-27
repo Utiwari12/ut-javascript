48db1c598c26f8badb6b280e04ad08831603965e
Utiwari12 <motioniitjeeinstitute@gmail.com>
2024-08-27 10:12:59 +0530

    objects

--- 02-basic/04-objects.js
+++ 02-basic/04-objects.js
@@ -9,7 +9,7 @@ user.name = "Upendra";
 user.age = 25;
 user.email = "X6fUz@example.com";
 
-console.log(user);
+//console.log(user);
 
 const regularUser = {
     email: "ut@gmail.com",
@@ -31,10 +31,44 @@ const obj2 = {4: "a", 5: "b", 6: "c"};
 //const obj3 = Object.assign(obj1, obj2); // same o/p as above
 
 const obj3 = Object.assign({}, obj1, obj2); // same o/p as above
-console.log(obj3);
+// console.log(obj3);
 
-console.log(Object.keys(user));
-console.log(Object.values(user));
-console.log(Object.entries(user));
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+// console.log(Object.entries(user));
 
-console.log(user.hasOwnProperty("email"));
+// console.log(user.hasOwnProperty("email"));
+
+//console.log(Object.is(obj1, obj2));
+
+//destructuring of object
+
+// const course = {
+//     coursename: "JavaScript",
+//     price: 100,
+//     courseInstructor: "Upendra Tiwari",
+//     courseStudents: ["Upendra", "Nilam", "Rajesh"],
+// }
+
+//const {coursename, price, courseInstructor: instructor, courseStudents} = course;
+//console.log(instructor);
+
+ //const navbar = ({company}) => {} //destructuring
+
+//navbar(company = "Google");
+
+//console.log({company});
+
+//json => javascript object notation
+//API => application programming interface
+// {
+//     "name": "Upendra",
+//     "coursename": "JavaScript",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
