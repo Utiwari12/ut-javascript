69b5a5e331de29caf2dc26df6a56745ba448a33e
Utiwari12 <motioniitjeeinstitute@gmail.com>
2024-08-28 19:05:28 +0530

    do-while

---
+++ 05-loop iteration/02-whileLoop.js
@@ -0,0 +1,34 @@
+//while loop
+// let index =0;
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     //index++;
+//     index = index +2;
+    
+// }
+
+// let myArray = ["flash", "arrow", "superman", "batman"];
+// let arr = 0;
+// while (arr < myArray.length) {
+//     //console.log(myArray[arr]);
+//     console.log(`Value of arr is ${myArray[arr]}`);
+//     //arr++;
+//     arr = arr + 1;
+    
+// }
+
+//do while loop
+
+let score = 1;
+
+do {
+    console.log(`Value of score is ${score}`);
+    score++;
+} while (score <= 10);
+
+let marks = 11;
+
+do {
+    console.log(`Value of score is ${marks}`);
+    marks++;
+} while (marks <= 10);
